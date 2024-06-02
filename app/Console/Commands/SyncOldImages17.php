0102035bbcb67d62d9231b0f52a1c6ba5dba1cec
ahmedabdelraouf <ahmednsreldeen94@gmail.com>
2024-06-03 00:09:44 +0300

    reorder album images

--- app/Console/Commands/SyncOldImages17.php
+++ app/Console/Commands/SyncOldImages17.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Http\Controllers\Admin\AlbumsController;
+use App\Models\AlbumImages;
 use Illuminate\Console\Command;
 
 class SyncOldImages17 extends Command
@@ -38,10 +39,29 @@ class SyncOldImages17 extends Command
      */
     public function handle()
     {
-        $AlbumsController = new AlbumsController();
-        $date = $this->getDate();
-        $AlbumsController->importOldNewsImagesToStorage($date);
+        $this->reorderAlbumImages();
         return 0;
+    }
+
+    function reorderAlbumImages()
+    {
+        // Retrieve all unique album IDs from album_images
+        $albumIds = AlbumImages::where("album_id",4)->distinct()->pluck('album_id');
+
+        // Iterate through each album
+        foreach ($albumIds as $albumId) {
+            // Retrieve images for the current album ordered by 'order'
+            $images = AlbumImages::where('album_id', $albumId)->orderBy('order')->get();
+
+            // Calculate the new order for each image
+            $imageCount = $images->count();
+            foreach ($images as $image) {
+                $newOrder = $imageCount - $image->order + 1; // Reverse the order
+                $image->order = $newOrder;
+                $image->save();
+            }
+        }
+        return 1;
     }
 
     public function getDate()
